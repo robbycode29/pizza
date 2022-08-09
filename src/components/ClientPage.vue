cb2d7eb8764bea15ae7aa554567f892b0acbea05
robbycode29 <robertandrei.oprescu@gmail.com>
2022-08-09 16:12:44 +0200

    added project

---
+++ src/components/ClientPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex flex-row h-full">  
+        <SideNavBar></SideNavBar>
+        <router-view class="w-screen"></router-view>
+    </div>
+</template>
+
+<script>
+import SideNavBar from './client_page-components/SideNavBar.vue';
+
+export default {
+    name: 'ClientPage',
+    components: {
+    SideNavBar
+}
+}
+
+</script>
